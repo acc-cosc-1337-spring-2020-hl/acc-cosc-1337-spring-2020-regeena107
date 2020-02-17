f07c16351ac2d2686b2c71496300c7af5e2fd2d6
regeena107 <regeena107@gmail.com>
2020-02-16 21:13:15 -0600

    Test cases for homework

--- test/homework_test/02_decisions_test/02_decisions_tests.cpp
+++ test/homework_test/02_decisions_test/02_decisions_tests.cpp
@@ -1,7 +1,5 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
-#include "decisions.h"
-#include "decisions.cpp"
+#include"decisions.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
