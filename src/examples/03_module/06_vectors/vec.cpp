8ff8638fa72498da5a8f20c4cac44dc9a0a2930d
regeena107 <regina.miranda@g.austincc.edu>
2020-02-17 18:56:12 -0600

    vectors

--- src/examples/03_module/06_vectors/vec.cpp
+++ src/examples/03_module/06_vectors/vec.cpp
@@ -1,6 +1,6 @@
-#include "for_ranged.h"
 #include<iostream>
 #include<vector>
+#include "vec.h"
 
 using std::vector;
 
@@ -10,13 +10,27 @@ The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows: 9 10 99 5  67
 */
 
-
+void loop_vector_w_index(vector<int> nums) 
+{
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		cout << nums[i] << "\n";
+	}
+}
 
 /*
 Write code for function loop_vector_w_index with a vector of int pass by value parameter.
 The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows: 9 10 99 5  67
 */
+void loop_vector_w_index_ref(vector<int> nums)
+{
+	for (auto n : nums)
+	{
+		cout << n << "\n";
+	}
+}
+
 
 
 
@@ -32,4 +46,12 @@ Write code for function loop_vector_w_index with a vector of int const pass by r
 The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows : 9 10 99 5  67
 */
+
+loop_vector_w_index_const(const vector<int>& nums)
+{
+	for (auto n : nums)
+	{
+		cout << n << "\n";
+	}
+}
 
--- test/examples_test/03_module_test/03_module_tests.cpp
+++ test/examples_test/03_module_test/03_module_tests.cpp
@@ -3,6 +3,10 @@
 #include "while.h"
 #include "value_ref.h"
 #include "value_ref.cpp"
+#include "for_ranged.h"
+#include <string>
+
+using std::string;
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -23,4 +27,20 @@ TEST_CASE("Test value and ref function")
 	REQUIRE(num1 == 5);
 	REQUIRE(num2 == 50);
 
+}
+
+TEST_CASE("Test for ranged loop by value")
+{
+	string name = "Joe";
+	loop_string_w_auto(name);
+
+	REQUIRE(name == "Joe");
+}
+
+TEST_CASE("Test for ranged loop by reference")
+{
+	string name = "Joe";
+	loop_string_w_auto_ref(name);
+
+	REQUIRE(name == "zzz");
 }
