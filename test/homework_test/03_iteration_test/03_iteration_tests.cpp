3670bbe2e3983383aee77315fd24fb85f655d787
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-02-20 20:55:54 -0600

    Update 03_iteration_tests.cpp

--- test/homework_test/03_iteration_test/03_iteration_tests.cpp
+++ test/homework_test/03_iteration_test/03_iteration_tests.cpp
@@ -1,7 +1,25 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define "dna.h"  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE ("Test GC content function")
+{
+    REQUIRE(get_gc_content(AGCTATAG) == 37.5);
+    REQUIRE(get_gc_content(CGCTATAG) == 50.0);
+}
+
+TEST_CASE ("Test reverse function")
+{
+    REQUIRE(get_reverse_string(AGCTATAG) == 'GATATCGA');
+    REQUIRE(get_gc_content(CGCTATAG) == GATATCGC);
+}
+
+TEST_CASE("Verify complement function")
+{
+    REQUIRE(get_dna_complement(AAAACCCGGT) == 'ACCGGGTTTT');
+    REQUIRE(get_dna_complement(CCCGGAAAAT) == 'ATTTTCCGGG');
+
+}
