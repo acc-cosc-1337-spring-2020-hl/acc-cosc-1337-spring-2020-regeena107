c51c04c0e97c68217bc63e9ce38d060f0853ce0e
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-02-20 20:54:49 -0600

    Update dna.cpp

--- src/homework/03_iteration/dna.cpp
+++ src/homework/03_iteration/dna.cpp
@@ -1,4 +1,7 @@
 #include "dna.h"
+
+using std::cout;
+
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -6,15 +9,24 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
+double get_gc_content (const string &dna)
+{
+    double count = sequence.length();
+    double gc_count =0;
 
 
-
+}
 
 /*
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
 
+string get_reverse_string(string dna)
+{
+
+    return string();
+}
 
 
 /*
@@ -28,3 +40,38 @@ c. return string
 
 */
 
+string get_dna_complement(string dna);
+
+{
+    get_reverse_string(dna);
+    string reg = dna;
+
+
+    if (dna[i]== 'G')
+    {
+        i == 'C';
+    }
+
+    else if (dna[i]=='C')
+    {
+        i=='G';
+    }
+
+    else if (dna[i]=='A')
+    {
+        i=='T';
+    }
+
+    else if (dna[i]=='T')
+    {
+        i=='A';
+    }
+
+    else
+    {
+        cout << "INVALID"
+    }
+
+    return string();
+
+}
