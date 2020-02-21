35d571efd22581a46f6fea477dd05d49cac355c4
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-02-20 20:55:20 -0600

    Update main.cpp

--- src/homework/03_iteration/main.cpp
+++ src/homework/03_iteration/main.cpp
@@ -1,7 +1,10 @@
 //write include statements
+#include "dna.h"
 
 //write using statements
-
+using std::string;
+using std::cout;
+using std::cin;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +15,23 @@ user enters a y or Y.
 */
 int main() 
 {
+    int option;
+    string sequence;
+
+    cout << "Enter 1 for Get GC Content or 2 for Get DNA Complement: ";
+    cin>> option;
+
+    cout << "Enter a DNA sequence: ";
+    cin>> sequence;
+
+    if (option == 1)
+    {
+         cout << get_gc_content(dna);
+    }
+
+    if (option == 2)
+    {
+        cout << get_gc_complement(dna);
+    }
 	return 0;
 }
