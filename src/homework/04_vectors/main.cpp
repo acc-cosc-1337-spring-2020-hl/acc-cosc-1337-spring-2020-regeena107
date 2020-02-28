93c582b591beec65a6a04609818ac80a9437805c
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-02-27 20:16:49 -0600

    Update main.cpp

--- src/homework/04_vectors/main.cpp
+++ src/homework/04_vectors/main.cpp
@@ -1,3 +1,11 @@
+#include "vectors.h"
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::cin;
+using std::string;
+
 /*
 use a vector of int with values 8, 4, 20, 88, 66, 99
 
@@ -9,6 +17,41 @@ Program continues until user decides to exit.
 */
 int main() 
 {
+    int choice;
+    cout << "Press 1 to get max from vectors.\n";
+    cout << "Press 2 to get primes.\n";
+    cout << "Press 3 to exit.\n";
+    cin >> choice;
 
-	return 0;
+    if (choice == 1)
+    {
+        vector<int> vec;
+        int i = 1;
+        while (i <= 5)
+        {
+            int num;
+            cout << "Num" << i << ":\n";
+            vec.push_back(num);
+            ++i;
+        }
+        int max = get_max_from_vector(vec);
+        cout << "Max number from vectors is: " << max << "\n";
+    }
+
+    else if
+        {
+        int num;
+        cout << "Enter a number: \n";
+        cin >> num;
+
+        vector<int> vector_of_primes(num);
+        cout << "List of primes: \n";
+
+        for (int prime : primes) {
+            cout << prime << "\n";
+        }
+        cout << "\n";
+        }
+
+return 0;
 }
