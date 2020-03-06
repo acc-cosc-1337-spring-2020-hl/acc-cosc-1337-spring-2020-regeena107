cbbcff29ddec53e91d6d116b0cd09f9933d762e0
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-03-05 23:47:57 -0600

    Update main.cpp

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,6 +1,33 @@
-
-int main() 
+int main()
 {
+    string first_player;
+    TicTacToe game;
+    cout << "First player, choose X or O: \n";
+    cin >> first_player;
+    
+    try
+    {
+        game.start_game(first_player);
+    }
+    
+    int position;
+    {
+        cout << "Enter a position between 1 through 9: \n";
+        cin >> position;
+       
+        try
+        {
+            game.mark_board(position);
+        }
+    
+    catch (error b)
+    {
+        cout << b.get_message();
+    }
+        cout << "\nEnter y to contine game. Enter n to exit game: \n";
+        cin >> choice;
+    }
 
-	return 0;
+   
+           return 0;
 }
