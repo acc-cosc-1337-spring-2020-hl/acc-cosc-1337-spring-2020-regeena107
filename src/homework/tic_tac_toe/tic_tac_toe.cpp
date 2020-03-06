2536fb5889dd48bb55834d16ea7c562e511266d7
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-03-05 19:57:58 -0600

    Update tic_tac_toe.cpp

--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,46 @@
 //cpp
+//cpp
+#include "tic_tac_toe.h"
+
+void TicTacToe::start_game(string first_player)
+{
+    if (first_player == "X" || first_player == "O")
+    {
+        first_player;
+    }
+    else
+    {
+        throw Error("\nPlayer must be X or O\n");
+    }
+    player = first_player;
+}
+
+void TicTacToe::mark_board(int position)
+{
+    if (position < 1 || position > 9)
+    {
+        throw Error("\nPostion must be 1 - 9\n");
+    }
+    else if (player == "")
+    {
+        throw Error("\nMust start game first\n");
+    }
+    else
+    {
+        set_next_player();
+    }
+}
+
+
+
+void TicTacToe::set_next_player()
+{
+    if (player == "X")
+    {
+        player = "O";
+    }
+    else
+    {
+        player = "X";
+    }
+}
