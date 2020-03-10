729febe56e4e551e43ad7f4bfd7767c4bb9e62be
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-03-09 19:37:26 -0500

    Update main.cpp

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,33 +1,31 @@
-#include "bank_account.h"
+#include "checking_account.h"	//include this now
 #include <iostream>
-#include<vector>
-
-using std::cout;
+#include <vector>
+using std::cout; 
 using std::vector;
-using std::cin;
 
 int main()
 {
-
-	BankAccount a(50), b(10);
-	//BankAccount c=a + b;
+	CheckingAccount c;
+	CheckingAccount a(50), b(10);
+	display_balance(a); 
+	cout << a;	//Overloading << operator
+	std::cin >> a; // overloading >> operator
 	display_balance(a);
-	cin >> a;
-	cout << a;
-
 	vector<BankAccount> accounts{ BankAccount(100),
-		BankAccount(200), BankAccount(300) };
+	BankAccount(200), BankAccount(300) };
 
 	for (auto act : accounts)
 	{
 		cout << act.get_balance() << "\n";
 	}
 
+
 	BankAccount account(500);
 	int balance = account.get_balance();
-	cout << "\nBalance: " << balance; 
+	cout << "\nbalance: " << balance;
 
-	try
+	try 
 	{
 		account.deposit(-10);
 	}
@@ -38,4 +36,3 @@ int main()
 
 	return 0;
 }
-
