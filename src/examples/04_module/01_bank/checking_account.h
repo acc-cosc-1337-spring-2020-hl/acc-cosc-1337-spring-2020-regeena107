e5c28b0a1152685fc264bcf389c146aa4def68a9
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-03-09 19:36:11 -0500

    Update checking_account.h

--- src/examples/04_module/01_bank/checking_account.h
+++ src/examples/04_module/01_bank/checking_account.h
@@ -1,1 +1,11 @@
+#include "bank_account.h"
 //checking_account.h
+class CheckingAccount : public BankAccount
+{
+
+public:
+	CheckingAccount() = default;
+	CheckingAccount(int b) : BankAccount(b) {}  // Initializing to a base class.
+	int get_balance()const { return balance * (1 + get_rate()); }
+  
+};
