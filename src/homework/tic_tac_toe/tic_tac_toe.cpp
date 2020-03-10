7da8940b67f2b7cde77d1e0905439b8fbbfea352
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-03-09 19:29:30 -0500

    Update tic_tac_toe.cpp

--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -1,46 +1,82 @@
 //cpp
-//cpp
 #include "tic_tac_toe.h"
-
-void TicTacToe::start_game(string first_player)
+#include <iostream>;
+using std::cout;
+bool TicTacToe::game_over()
 {
-    if (first_player == "X" || first_player == "O")
-    {
-        first_player;
-    }
-    else
-    {
-        throw Error("\nPlayer must be X or O.\n");
-    }
-    player = first_player;
+	return check_board_full();
+}
+
+void TicTacToe::start_game(std::string first_player)
+{
+	if (first_player == "X" || first_player == "O")
+	{
+		first_player;
+	}
+	else
+	{
+		throw Error("Player must be X or O\n");
+	}
+	player = first_player;
+	clear_board();
 }
 
 void TicTacToe::mark_board(int position)
 {
-    if (position < 1 || position > 9)
-    {
-        throw Error("\nPostion must be 1 - 9.\n");
-    }
-    else if (player == "")
-    {
-        throw Error("\nMust start game first.\n");
-    }
-    else
-    {
-        set_next_player();
-    }
+	if (position <1 || position > 9)
+	{
+		throw Error("Postion must be 1-9\n");
+	}
+	else if (player == "")
+	{
+		throw Error("Must start game first\n");
+	}
+	else
+	{
+		pegs[position - 1] = player;
+		set_next_player();	
+	}
+}
+
+void TicTacToe::display_board() const
+{
+	for (int i = 0; i < 9; i += 3)
+	{
+		cout << pegs[i] << "|" << pegs[i + 2] << "|" << pegs[i + 2] << "\n";
+
+	}
 }
 
 
 
+bool TicTacToe::check_board_full()
+{
+	for (auto peg : pegs)
+	{
+		if (peg == " ")
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
 void TicTacToe::set_next_player()
 {
-    if (player == "X")
-    {
-        player = "O";
-    }
-    else
-    {
-        player = "X";
-    }
+	if (player == "X")
+	{
+		player = "O";
+	}
+	else
+	{
+		player ="X";
+	}
+}
+
+void TicTacToe::clear_board()
+{
+	for (auto &peg : pegs)
+	{
+		peg = " ";
+	}
 }
