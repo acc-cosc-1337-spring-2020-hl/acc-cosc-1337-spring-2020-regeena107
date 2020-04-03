0b6a5e96093c524b44705728f97c07abc25326a9
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-04-02 19:25:53 -0500

    Update tic_tac_toe.h

--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,7 +1,9 @@
 //h
-#include <string>
-#include <vector>
-#include <iostream>
+#include<iostream>
+#include<string>
+#include<vector>
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
 
 using std::string; using std::vector;
 
@@ -11,22 +13,30 @@ public:
 	bool game_over();
 	void start_game(string first_player);
 	void mark_board(int position);
-	void display_board() const;
 	string get_player() const;
+	void display_board()const;
+	string get_winner(return winner);
 private:
+	bool check_column_win();
+	bool check_row_win();
+	bool check_diagnol_win();
+	void set_winner();
 	void set_next_player();
-	void clear_board();
 	bool check_board_full();
-	vector<string> pegs{ 9, " " };
 	string player;
+	string winner;
+	std::vector<string> pegs{ 9, " " };
+	void clear_board();
 };
-
 
 class Error
 {
-public:
+public: 
 	Error(string msg) : message{ msg } {};
 	string get_message();
 private:
 	string message;
+
 };
+
+#endif //!TIC_TAC_TOE_H
