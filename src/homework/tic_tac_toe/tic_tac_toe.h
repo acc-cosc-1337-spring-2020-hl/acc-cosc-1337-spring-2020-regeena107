4e357c328a12bfc91fe286bc7a282b4f93bccd70
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-04-02 19:34:23 -0500

    Update tic_tac_toe.h

--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,42 +1,36 @@
-//h
 #include<iostream>
 #include<string>
 #include<vector>
-#ifndef TIC_TAC_TOE_H
-#define TIC_TAC_TOE_H
 
-using std::string; using std::vector;
-
+using std::string;
+using std::cout;
 class TicTacToe
 {
 public:
 	bool game_over();
-	void start_game(string first_player);
 	void mark_board(int position);
-	string get_player() const;
-	void display_board()const;
-	string get_winner(return winner);
+	void start_game(string first_player);
+	string get_player() const { return player; }
+	void display_board() const;
+	string get_winner() { return winner; }
 private:
-	bool check_column_win();
-	bool check_row_win();
-	bool check_diagnol_win();
-	void set_winner();
-	void set_next_player();
 	bool check_board_full();
+	void set_next_player();
 	string player;
-	string winner;
 	std::vector<string> pegs{ 9, " " };
 	void clear_board();
+	string winner;
+	void set_winner();
+	bool check_row_win();
+	bool check_column_win();
+	bool check_diagonal_win();
 };
 
 class Error
 {
-public: 
+public:
 	Error(string msg) : message{ msg } {};
-	string get_message();
+	string get_message() const { return message; }
 private:
 	string message;
-
 };
-
-#endif //!TIC_TAC_TOE_H
