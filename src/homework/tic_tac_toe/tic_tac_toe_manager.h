373d5e051bf7cbb5543215c68dbe4c4a2604829a
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-04-23 18:27:04 -0500

    Update tic_tac_toe_manager.h

--- src/homework/tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/tic_tac_toe/tic_tac_toe_manager.h
@@ -1,1 +1,23 @@
 //h
+#include"tic_tac_toe.h"
+#include<memory>
+#ifndef TIC_TAC_TOE_MANAGER_H
+#define TIC_TAC_TOE_MANAGER_H
+
+using namespace std;
+
+class tic_tac_toe_manager
+{
+public:
+	void save_game(TicTacToe b);
+	friend std::ostream& operator<<(std::ostream& out, const tic_tac_toe_manager& manager);
+	vector unique_ptr<TicTacToe> games;
+  
+private:
+	int x_win = 0;
+	int o_win = 0;
+	int ties = 0;
+	void update_winner_count(string winner);
+};
+
+#endif // !TIC_TAC_TOE_MANAGER_H
