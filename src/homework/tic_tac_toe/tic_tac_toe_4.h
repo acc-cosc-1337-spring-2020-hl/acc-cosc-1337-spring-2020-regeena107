e7ba2a9a0e6d0cfb28235982c6d7002d6c49e9d7
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-04-23 18:19:56 -0500

    Update tic_tac_toe_4.h

--- src/homework/tic_tac_toe/tic_tac_toe_4.h
+++ src/homework/tic_tac_toe/tic_tac_toe_4.h
@@ -1,1 +1,19 @@
 //h
+#include"tic_tac_toe.h"
+#ifndef TIC_TAC_TOE_H_4
+#define TIC_TAC_TOE_H_4
+
+class TicTacToe4 : public TicTacToe
+{
+public:
+	TicTacToe4() : TicTacToe(4) {};
+	friend std::ostream& operator<<(std::ostream & out, TicTacToe4 &game);
+	friend std::istream& operator>>(std::istream & in, TicTacToe4 &game);
+
+private:
+	bool check_colunm_win();
+	bool check_row_win();
+	bool check_diagonal_win(); 
+};
+
+#endif // !TIC_TAC_TOE_H_4
