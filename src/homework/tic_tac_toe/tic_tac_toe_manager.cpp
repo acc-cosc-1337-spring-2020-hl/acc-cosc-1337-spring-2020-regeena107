57b9eb3f62f8e55aed63a6a0ce68444b3ff370a7
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-04-23 18:40:15 -0500

    Update tic_tac_toe_manager.cpp

--- src/homework/tic_tac_toe/tic_tac_toe_manager.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe_manager.cpp
@@ -1,1 +1,42 @@
 //cpp
+#include "tic_tac_toe_manager.h"
+
+void TicTacToeManager::save_game(TicTacToe b)
+{
+	games.push_back(b);
+	update_winner_count(b.get_winner());
+    get_winner_total(x_win, o_win, ties);
+
+}
+
+void TicTacToeManager::get_winner_total(int& x, int& o, int& t)
+{
+    cout << "\nX wins " << x << ", O wins " << o << ", Ties " << t;
+}
+
+void TicTacToeManager::update_winner_count(string winner)
+{
+	if (winner == "X") {
+		x_win++;
+	}
+	else if (winner == "O") {
+		o_win++;
+	}
+	else {
+		ties++;
+	}
+}
+
+std::ostream & operator<<(std::ostream & out, const TicTacToeManager & manager)
+{
+	for (auto game : manager.games)
+    {
+        out << game;
+    }
+
+	out << "X Win Count: " << manager.x_win << "\n";
+	out << "O Win Count: " << manager.o_win << "\n";
+	out << "Tie Count: " << manager.ties << "\n";
+    
+	return out;
+}
