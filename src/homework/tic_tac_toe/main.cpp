b0456125306b696367155d1d8322a717c5e2207d
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-04-23 18:37:29 -0500

    Update main.cpp

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,47 +1,81 @@
-#include "tic_tac_toe.h"
+#include<iostream>
+#include<functional>
+#include "tic_tac_toe_manager.h"
+#include "tic_tac_toe_3.h"
+#include "tic_tac_toe_4.h"
 
-
-using std::cout;
-using std::cin;
+using std::cout; 
+using std::cin; 
 using std::string;
 
-int main() 
+int main()
 {
-	int option = 1;
-	int position;
-	while (option == 1)
+	tic_tac_toe_manager manager;
+	string cont;
+	std::vector<std::reference_wrapper<TicTacToe>> games;
+
+	do
 	{
-		string first_player;
-		TicTacToe game;
-		while (!(first_player == "O" || first_player == "X"))
-		{
-			cout << "Please choose one: 'X' or 'O'\n";
-			cin >> first_player;
-			try {
-				game.start_game(first_player);
-			}
-			catch (Error e) {
+		int game_type;
+		cout << "\nTictactoe 3 or 4?";
+		cin >> game_type;
+		TicTacToe3 game3;
+		TicTacToe4 game4;
 
+		if (game_type == 3)
+		{
+			games.push_back(game3);
+		}
+		else if (game_type == 4)
+		{
+			games.push_back(game4);
+		}
+
+		std::reference_wrapper<TicTacToe> game = games.back();
+
+		string player = "Y";
+
+		while (!(player == "O" || player == "X"))
+		{
+			try
+			{
+				cout << "Enter player: ";
+				cin >> player;
+
+				game.get().start_game(player);
+			}
+			catch (error e)
+			{
 				cout << e.get_message();
 			}
 		}
 
-		while (!game.game_over())
+		int choice = 1;
+
+		do
 		{
-			cout << "Please enter a position \n";
-			cin >> position;
-			try {
-				game.mark_board(position);
+			try
+			{
+				cin >> game.get();
+				cout << game.get();
 			}
-			catch (Error e) {
+			catch (error e)
+			{
 				cout << e.get_message();
 			}
-			game.display_board();
-		}
-		cout << "\nThe winner is: " <<game.get_winner()<< "\n" ;
-		cout << "Enter '1' to continue with the game or '2' to finish.\n";
-		cin >> option;
-	}
+
+		} while (!game.get().game_over());
+
+		manager.save_game(game.get());
+
+		cout << "\nWinner: " << game.get().get_winner() << "\n";
+
+		cout << "Enter Y to play again: ";
+		cin >> cont;
+
+	} while (cont == "Y");
+
+	cout << manager;
 
 	return 0;
 }
