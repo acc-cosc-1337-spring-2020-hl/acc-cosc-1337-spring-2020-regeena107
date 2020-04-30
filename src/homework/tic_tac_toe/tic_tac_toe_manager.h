fd386c84d5e6aaec50b4ef7135abb3107e0490c6
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-04-30 16:11:31 -0500

    homework 11

--- src/homework/tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/tic_tac_toe/tic_tac_toe_manager.h
@@ -1,24 +1,31 @@
 //h
-#include"tic_tac_toe.h"
-#include<memory>
+#include<iostream>
 #include<vector>
+#include "tic_tac_toe.h"
+#include "tic_tac_toe_3.h"
+#include "tic_tac_toe_data.h"
+#include <memory>
+
+using std::vector;
+using std::unique_ptr;
+
 #ifndef TIC_TAC_TOE_MANAGER_H
 #define TIC_TAC_TOE_MANAGER_H
 
-using namespace std;
-
-class TicTacToeManager
-{
+class TicTacToeManager {
 public:
-	void save_game(std::unique_ptr<TicTacToe>& b);
-	friend std::ostream& operator<<(std::ostream& out, const TicTacToeManager& manager);
-	std::vector< unique_ptr<TicTacToe>> games;
-  
-private:
-	int x_win = 0;
-	int o_win = 0;
+	TicTacToeManager() = default;
+	TicTacToeManager(TicTacToeData& d);
+	void save_game(unique_ptr<TicTacToe>& game);
+	friend std::ostream& operator<<(std::ostream & out, const TicTacToeManager &manager);
+
+	private:
+	vector<unique_ptr<TicTacToe>> games;
+	int x_wins = 0;
+	int o_wins = 0;
 	int ties = 0;
 	void update_winner_count(string winner);
+	TicTacToeData data;
 };
 
 #endif // !TIC_TAC_TOE_MANAGER_H
