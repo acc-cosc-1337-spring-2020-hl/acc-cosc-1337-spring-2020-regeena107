afd8ea0ca961cdc444c59eef7ab6b0220c87c8bf
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-04-30 16:09:06 -0500

    homework 11

--- src/homework/tic_tac_toe/tic_tac_toe_data.h
+++ src/homework/tic_tac_toe/tic_tac_toe_data.h
@@ -1,1 +1,16 @@
 //h
+#include "tic_tac_toe.h"
+#include <vector>
+#include <string>
+#include<fstream>
+#include<iostream>
+
+class TicTacToeData
+{
+public:
+	void save_pegs(const std::vector<std::unique_ptr<TicTacToe>>& games);
+	std::vector<std::unique_ptr<TicTacToe>> get_games()const;
+private:
+	const std::string file_name{ "TicTacToe_games.dat" };
+};
+#endif // !TIC_TAC_TOE_DATA_H
