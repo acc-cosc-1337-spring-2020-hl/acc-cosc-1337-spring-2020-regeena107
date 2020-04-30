f829793274db5637d01fe87be4d02d5b8f171070
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-04-30 16:02:25 -0500

    homework 11

--- src/homework/tic_tac_toe/tic_tac_toe_4.h
+++ src/homework/tic_tac_toe/tic_tac_toe_4.h
@@ -1,19 +1,22 @@
 //h
-#ifndef TIC_TAC_TOE_4
-#define TIC_TAC_TOE_4
 #include "tic_tac_toe.h"
-#include<string>
-#include<vector>
+#ifndef TIC_TAC_TOE_H_4
+#define TIC_TAC_TOE_H_4
 
-class TicTacToe4 : public TicTacToe
+class TicTacToe4 : public TicTacToe 
 {
 public:
-	TicTacToe4() : TicTacToe(4) {}
+	TicTacToe4() :TicTacToe(4) {}
+	TicTacToe4(std::vector<string> p, string win) : TicTacToe(p, win) {};
+	friend std::ostream& operator<<(std::ostream & out, TicTacToe4 &game);
+	friend std::istream& operator>>(std::istream & in, TicTacToe4 &game);
+
 private:
+
 	bool check_column_win() override;
 	bool check_row_win() override;
 	bool check_diagonal_win() override;
 
 };
 
-#endif // !TIC_TAC_TOE_3
+#endif // !TIC_TAC_TOE_H_4
