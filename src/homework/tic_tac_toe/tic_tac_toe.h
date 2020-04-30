8558c4062281e72b8130c49bf616056ba8198ba9
regeena107 <59001371+regeena107@users.noreply.github.com>
2020-04-30 15:59:23 -0500

    homework 11

--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,48 +1,49 @@
-#include<iostream>
-#include<string>
-#include<vector>
 #ifndef TIC_TAC_TOE_H
 #define TIC_TAC_TOE_H
+#include <string>
+#include<vector>
 
 using std::string;
-using std::cout;
 
-class TicTacToe
-{
+class TicTacToe {
+
 public:
-	TicTacToe(int s) : pegs(s*s, " ") {}
-	bool game_over();
-	void mark_board(int position);
+	TicTacToe(std::vector<string> p, string win) : pegs{p}, winner{win} {}
+	explicit TicTacToe(int s) : pegs(s*s, " ") {}
 	void start_game(string first_player);
-	string get_player() const { return player; }
-	void display_board() const;
+	void mark_board(int position);
+	string get_player() const{ return player;}
+	bool game_over();
 	string get_winner() { return winner; }
-	friend std::ostream& operator<<(std::ostream& out, const TicTacToe& t);
-	friend std::istream& operator>>(std::istream& in, TicTacToe& t);
-
-protected:
-	virtual bool check_row_win() = 0;
-	virtual bool check_column_win()=0;
-	virtual bool check_diagonal_win()=0;
-	std::vector<string> pegs{ 9, " " };
-
-private:
-	bool check_board_full();
-	string player;
-	string winner;
-	void set_next_player();
-	void set_winner();
-	void clear_board();
+	std::vector<string> get_pegs() const { return pegs; }
+	friend std::ostream& operator<<(std::ostream & out, TicTacToe &game);
+	friend std::istream& operator>>(std::istream & in, TicTacToe &game);
 	
-};
+protected:
+	std::vector<string> pegs;
+	string player = "";
+	virtual bool check_column_win() = 0;
+	virtual bool check_row_win()  = 0;
+	virtual bool check_diagonal_win() = 0;
 
-class Error
-{
-public:
-	Error(string msg) : message{ msg } {};
-	string get_message() { return message; }
 private:
-	string message;
+
+   	void set_next_player();
+	bool check_board_full();
+	void clear_board();
+	void set_winner();
+	string winner= "C";
+	bool tie = false;
+
 };
 
-#endif // !TIC_TAC_TOE_H
+class XOException{
+
+public:
+    XOException(string msg) : message{ msg } {}
+    string get_message() const { return message; }
+private:
+    string message;
+
+};
+#endif // TIC_TAC_TOE_H
